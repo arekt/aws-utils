3c176136bf5473af75f6a1bc585a8484f34d910a
Arek Turlewicz <arek@turlewicz.com>
2019-05-07 00:36:45 +0900

    WIP but works.

--- src/resource_transform.rb
+++ src/resource_transform.rb
@@ -1,16 +1,17 @@
 require 'json'
+require 'yaml'
 require "active_support/all"
-
 
 module ResourceTransform
   def macro(event:, context:)
     fragment = event.dig("fragment")
     requestId = event.dig("requestId")
-    {
+    output = {
       fragment: transform(fragment),
       status: "success",
       requestId: requestId
     }.deep_stringify_keys
+    output
   end
 
   private
@@ -19,15 +20,24 @@ module ResourceTransform
     base_name = resource["Type"]
     return yield(resource) unless base_name.present?
     file_name = "templates/#{base_name.underscore}.yaml.erb"
-    yield(YAML.load_file(file_name)) if File.exist?(file_name)
+    if File.exist?(file_name)
+      yield(YAML.load_file(file_name))
+    else
+      yield(resource) #TODO maybe better to check if Type match "Simple"
+    end
   end
 
   def transform(fragment)
+    puts "-- original --"
+    puts fragment
     r = fragment["Resources"].map do |key, resource|
       content_for_resource(resource) do |content|
         [ key, content ]
       end
     end
-    Hash[r]
+    output = {Resources: Hash[r]}
+    puts "-- result --"
+    puts output
+    output
   end
 end
